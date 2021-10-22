e2b9fad7ac1ce933269e1573ddbd78ee37da5a8a
Carl Avidano <cavidano@gmail.com>
2021-10-22 14:13:07 -0400

    added expenses component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,18 +2,14 @@ import { Fragment } from 'react';
 
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
-
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Isurance';
-    const expenseAmount = 294.67;
+const ExpenseItem = (props) => {
 
     return (
         <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <div>{expenseTitle}</div>
-                <div className="expense-item__price">${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     )
